e374ae4123a7144532cb47e9509fb12ea73cf50d
Dawid Nowak <dawid.nowak040@gmail.com>
2023-04-13 17:35:15 +0200

    extract data update

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 function search(){
     const location = document.querySelector('.location').value;
 
-    const fetchPromise = fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/New%20York%20City%2CNY?unitGroup=us&key=KVLNCPER3AFYHKHH7V37JH8T8&contentType=json');
+    const fetchPromise = fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/WALBRZYCH?unitGroup=us&key=KVLNCPER3AFYHKHH7V37JH8T8&contentType=json');
 
     fetchPromise.then((response) => {
         const jsonPromise = response.json();
         jsonPromise.then((data) => {
-            console.log(data.address);
+            console.log(data.resolvedAddress)
 
-            console.log('');
-            console.log('currentConditions');
+            console.log('')
+            console.log('currentConditions')
 
-            console.log(data.currentConditions.cloudcover);
-            console.log(data.currentConditions.conditions);
-            console.log(data.currentConditions.datetime);
-            console.log(data.currentConditions.temp);
-            console.log(data.currentConditions.uvindex);
-            console.log(data.currentConditions.windspeed);
+            console.log(data.alerts[0])
+            console.log(data.description)
+            
+
+            console.log(data.currentConditions.cloudcover)
+            console.log(data.currentConditions.conditions)
+            console.log(data.currentConditions.datetime)
+            console.log(data.currentConditions.temp)
+            console.log(data.currentConditions.uvindex)
+            console.log(data.currentConditions.windspeed)
 
             for(let i = 0; i < 15; i++){
-                console.log('');
-                console.log('days', i+1);
+                console.log('')
+                console.log('day', i+1)
 
-                console.log(data.days[i].cloudcover);
-                console.log(data.days[i].conditions);
-                console.log(data.days[i].datetime);
-                console.log(data.days[i].temp);
-                console.log(data.days[i].uvindex);
-                console.log(data.days[i].windspeed);
+                console.log(data.days[i].cloudcover)
+                console.log(data.days[i].conditions)
+                console.log(data.days[i].datetime)
+                console.log(data.days[i].temp)
+                console.log(data.days[i].uvindex)
+                console.log(data.days[i].windspeed)
             };
         });
     });
