245777ecfdfa2b5af40d4950dfb6e86c7d694d53
Dawid Nowak <dawid.nowak040@gmail.com>
2023-04-20 09:22:04 +0200

    logn term forecast and temp unit changing

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 function search(){
     const location = document.querySelector('#location').value;
-    const unit = document.querySelector('#unit').value;
+    let unit = document.querySelector('#unit').value;
     const apiKey = "KVLNCPER3AFYHKHH7V37JH8T8";
     const apiRequest = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${unit}&key=${apiKey}`;
 
+    if(unit === 'metric'){
+        unit = 'C';
+    }
+    else{
+        unit = 'F';
+    };
+    
     fetch(apiRequest)
     .then(res => {
         if(!res.ok){
@@ -18,14 +25,14 @@ function search(){
         document.querySelector('main').innerHTML = '<img src="logo.svg" alt="" class="logo"><form><label for="location"><h2>Enter a valid location!</h2></label><select id="unit"><option value="metric">Metric</option><option value="us">US</option></select><div class="search"><input type="text" id="location"><button onclick="search()">Search</button></div></form>'
     })
     .then((res) =>{
-        forecast(res);
+        shortTermForecast(res, unit);
     });
 
     document.querySelector("main").innerHTML = '<img src="logo.svg" alt=""><div class="forecast"></div>';
-    document.querySelector(".forecast").innerHTML = '<div class="aa"><div id="date"></div><div id="adress"></div></div><div class="aa"><div id="temp"></div></div><div class="aa"><div id="wind"></div><div id="uv"></div></div><div class="aa"><div id="cloud"></div></div>';
+    document.querySelector(".forecast").innerHTML = '<div class="aa"><div id="date"></div><div id="adress"></div></div><div class="aa"><div id="temp"></div></div><div class="aa"><div id="wind"></div><div id="uv"></div></div><div class="aa"><div id="cloud"></div></div><h2>Show long-term forecast</h2>';
 };
 
-function forecast(res){
+function shortTermForecast(res, unit){
     document.querySelector("#date").innerText += res.currentConditions.datetime;
     document.querySelector("#adress").innerText +=  res.resolvedAddress;
 
@@ -33,7 +40,7 @@ function forecast(res){
     console.log(res.description)
 
     document.querySelector("#temp").innerText += res.currentConditions.temp;
-    document.querySelector("#temp").innerText += " C/F";
+    document.querySelector("#temp").innerText += ` ${unit}`;
 
     document.querySelector("#wind").innerText += res.currentConditions.windspeed;
     document.querySelector("#wind").innerText += " Windspeed";
@@ -43,15 +50,15 @@ function forecast(res){
     document.querySelector("#cloud").innerText += res.currentConditions.cloudcover;
     document.querySelector("#cloud").innerText += res.currentConditions.conditions;
 
-    for(let i = 0; i < 15; i++){
-        console.log('')
-        console.log('day', i+1)
+    document.querySelector('h2').addEventListener("click", () =>{
+        longTermForecast(res, unit);
+    });
+};
 
-        console.log(res.days[i].cloudcover)
-        console.log(res.days[i].conditions)
-        console.log(res.days[i].datetime)
-        console.log(res.days[i].temp)
-        console.log(res.days[i].uvindex)
-        console.log(res.days[i].windspeed)
+function longTermForecast(res, unit){
+    document.querySelector(".forecast").innerHTML = '';
+
+    for(let i = 0; i < 15; i++){
+        document.querySelector('main').innerHTML += `<h2>Day ${i+1}</h2><div>${res.days[i].datetime}</div><div>${res.days[i].temp} ${unit}</div><div>${res.days[i].windspeed} Windspeed</div><div>${res.days[i].uvindex} UV</div><div>${res.days[i].cloudcover}</div><div>${res.days[i].conditions}</div>`;
     };
 };
